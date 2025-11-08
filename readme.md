6653a0f788e73e642a2f4b9a9bdb04e448cd6344
Jason Cavalere <jasoncavaliere@hotmail.com>
2025-11-07 18:13:14 -0600

    updating RG and params files.

--- readme.md
+++ readme.md
@@ -1,7 +1,7 @@
 # Requirements
 [.NET CLI](https://learn.microsoft.com/en-us/dotnet/core/tools/)
 [Azure Functions Core Tools CLI](https://learn.microsoft.com/en-us/azure/azure-functions/functions-run-local?tabs=windows%2Cisolated-process%2Cnode-v4%2Cpython-v2%2Chttp-trigger%2Ccontainer-apps&pivots=programming-language-csharp)
-
+[Azure CLI](https://learn.microsoft.com/en-us/cli/azure/get-started-with-azure-cli?view=azure-cli-latest)
 # Run instructions
 ```
 dotnet build
--- src/FunctionApp/obj/Debug/net8.0/FunctionApp.AssemblyInfo.cs
+++ src/FunctionApp/obj/Debug/net8.0/FunctionApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FunctionApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5ab680914c25f871ed293ba4d3e49167a66e39d")]
 [assembly: System.Reflection.AssemblyProductAttribute("FunctionApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FunctionApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
